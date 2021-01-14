0f0c63ab2343253ce749c5472cfae1390e3804b6
Kevin Gabriel Alves de Melo <kevinalvesmelo@gmail.com>
2021-01-14 16:45:16 -0300

    Promoção de peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,12 @@ public class Program {
 				if(capturedPiece != null ) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Digita a peca para ser promovida!   (B/H/R/Q):  ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e){
 				System.out.println(e.getMessage());
